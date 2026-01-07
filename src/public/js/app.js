b370813b825d42438783227b2a79b0f898ea6c53
yebeen <dpqls0356@gmail.com>
2026-01-07 15:00:30 +0900

    study:  express + ws 기반 단순한 채팅 기능 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,15 +1,65 @@
+const messageList = document.querySelector("#messageList");
+const messageForm = document.querySelector("#mesaageForm");
+const nameForm = document.querySelector("#saveNameForm");
+let name;
+
 const socket = new WebSocket(`ws://${window.location.host}`);
+/**
+ * message type1
+ * {
+ *  type:"message",
+ *  message:"~~",
+ *  name: name
+ * }
+ *
+ * message type2
+ * {
+ *  type:"name"
+ *   name:"~~~"
+ * }
+ */
+const makeMessageObj = (type, message, name) => {
+  return JSON.stringify({ type, message, name });
+};
+const makeNameObj = (type, name) => {
+  return JSON.stringify({ type, name });
+};
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server✅");
 });
-socket.addEventListener("message", (message) => {
-  console.log("New Message : ", message.data);
-});
+
 socket.addEventListener("close", () => {
-  consoel.log("Disconnected from Server😭");
+  console.log("Disconnected from Server😭");
 });
 
-setTimeout(() => {
-  socket.send("Hello Server!");
-}, 1000);
+socket.addEventListener("message", (event) => {
+  const { type, payload } = JSON.parse(event.data);
+  if (type === "message") {
+    const li = document.createElement("li");
+    li.innerHTML = payload.name + " : " + payload.message;
+    messageList.appendChild(li);
+  } else if (type === "addName_error") {
+    alert(payload);
+  } else if (type === "addName_success") {
+    alert("채팅가능");
+    nameForm.querySelector("input").value = "";
+  }
+});
+
+const handleSubmit = (evnet) => {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessageObj("message", input.value, name));
+  input.value = "";
+  console.log("handleSubmit");
+};
+const handleName = (event) => {
+  event.preventDefault();
+  name = nameForm.querySelector("input").value;
+  socket.send(makeNameObj("name", name));
+  console.log("handleName");
+};
+
+messageForm.addEventListener("submit", handleSubmit);
+nameForm.addEventListener("submit", handleName);
--- src/server.js
+++ src/server.js
@@ -18,14 +18,71 @@ const server = http.createServer(app); //http 서버
 const wss = new WebSocket.Server({ server }); //http서버 위에 ws 서버를 올림
 server.listen(3000, handleListen);
 
+const users = new Map(); // 연결된 소켓 정보
+const nameToSocket = new Map(); // 이름으로 소켓 검색 시 사용
+
+const sendMessage = (data, senderSocket) => {
+  const senderName = users.get(senderSocket)?.name;
+  for (const socket of users.keys()) {
+    // if (socket === senderSocket) continue;
+    socket.send(
+      JSON.stringify({
+        type: "message",
+        payload: {
+          message: data.message,
+          name: senderName,
+        },
+      })
+    );
+  }
+};
+const addUser = (data, socket) => {
+  if (nameToSocket.has(data.name)) {
+    socket.send(
+      JSON.stringify({
+        type: "addName_error",
+        payload: "중복된 이름입니다.",
+      })
+    );
+  }
+  const prevName = users.get(socket)?.name;
+  if (prevName) {
+    nameToSocket.delete(prevName);
+  }
+
+  users.get(socket).name = data.name;
+  nameToSocket.set(data.name, socket);
+
+  socket.send(
+    JSON.stringify({
+      type: "addName_success",
+      payload: users.get(socket).name,
+    })
+  );
+};
+
 //connection이 작동되면 함수가 실행되고 이 떄 sockect[백엔드에 연결된 사람의 정보를 담음]을 리턴받음
 wss.on("connection", (socket) => {
+  users.set(socket, { name: null });
   console.log("Connected from the browser");
-  socket.send("Hi User!");
+
   socket.on("close", () => {
+    const name = users.get(socket)?.name;
+    if (name) nameToSocket.delete(name);
+    users.delete(socket);
     console.log("Disconnected from the browser😭");
   });
-  socket.on("message", (message) => {
-    console.log("New Message : ", message.toString("utf8"));
+
+  socket.on("message", (data) => {
+    const parsedData = JSON.parse(data.toString("utf8"));
+    console.log(parsedData);
+    switch (parsedData.type) {
+      case "name":
+        addUser(parsedData, socket);
+        break;
+      case "message":
+        sendMessage(parsedData, socket);
+        break;
+    }
   });
 });
